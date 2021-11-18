cd05b6d73694439da1d4d15b6fc63dd624381926
johnsonjuvenary <johnsonjuvenary@gmail.com>
2021-11-18 11:13:00 +0300

    first commit

---
+++ components/PageNotFound.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>Page Not Found</h3>
+    <img :src="require('@/assets/images/fe25.jpg')" alt="" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50rem;
+}
+h3 {
+  font-size: 3rem;
+}
+img {
+  width: 15rem;
+  margin: 0 auto;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Hero />
+    <br /><br /><br /><br />
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
+    <smallCardDisplay
+      v-for="cardInfo in smallCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
+  </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from "@/assets/data.js";
+export default {
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
